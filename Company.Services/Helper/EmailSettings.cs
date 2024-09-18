8e99446ca782be0035c3991083b4046224ed8bb3
AhmadMorshed <ahmadnakawa676@gmail.com>
2024-09-18 05:35:43 -0700

    first commit

---
+++ Company.Services/Helper/EmailSettings.cs
@@ -0,0 +1,23 @@
+﻿using Company.Data.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Services.Helper
+{
+    public static class EmailSettings
+    {
+        public static void SendEmail(Email input)
+        {
+            var client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential("ahmadnakawa676@gmail.com\r\n","lagcddtaxfekbomj");
+            client.Send("ahmadnakawa676@gmail.com", input.To , input.Subject , input.Body);
+        }
+
+    }
+}
